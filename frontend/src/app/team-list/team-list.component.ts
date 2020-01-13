d062dbb6c0e1e5143e071743b86842f1ef1d1073
Kasky18 <martinbrazina18@gmail.com>
2020-01-13 16:45:13 +0100

    completed restController + adding home&away table lists

--- frontend/src/app/team-list/team-list.component.ts
+++ frontend/src/app/team-list/team-list.component.ts
@@ -25,7 +25,7 @@ export class TeamListComponent implements OnInit {
   ngOnInit() {
     // this.getNumber(1);
     // this.reloadData();
-    this.showTableOfTeams();
+    this.showOverallTableOfTeams();
   }
 
 
@@ -43,7 +43,7 @@ export class TeamListComponent implements OnInit {
   }
 
 
-  showTableOfTeams() {
+  showOverallTableOfTeams() {
     this.teamService.getTeamsList()
       .pipe(
         mergeMap(res => {
@@ -59,7 +59,102 @@ export class TeamListComponent implements OnInit {
                 this.gameService.getPointsByTeam(teamId),
                 this.gameService.getGoalsForTeam(teamId),
                 this.gameService.getGoalsAgainstTeam(teamId),
-                this.gameService.getGamesPlayedByTeam(teamId)
+                this.gameService.getNumberOfGamesPlayedByTeam(teamId)
+              ]).pipe(
+                map(data => {
+                  // @ts-ignore
+                  teamTest.wins = data[0];
+                  // @ts-ignore
+                  teamTest.loses = data[1];
+                  // @ts-ignore
+                  teamTest.overtimeLoses = data[2];
+                  // @ts-ignore
+                  teamTest.points = data[3];
+                  // @ts-ignore
+                  teamTest.goalsFor = data[4];
+                  // @ts-ignore
+                  teamTest.goalsAgainst = data[5];
+                  // @ts-ignore
+                  teamTest.gamesPlayed = data[6];
+                  return teamTest;
+                })
+              )
+            }),
+            toArray()
+          );
+        })
+      )
+      .subscribe(value => {
+        this.teamTest = value;
+        console.log(value)
+      });
+  }
+
+  showHomeTableOfTeams() {
+    this.teamService.getTeamsList()
+      .pipe(
+        mergeMap(res => {
+          return from(res).pipe(
+            mergeMap(teamTest => {
+
+              // @ts-ignore
+              const {teamId} = teamTest;
+              return forkJoin([
+                this.gameService.getGameStatusByHomeTeam(teamId, "win"),
+                this.gameService.getGameStatusByHomeTeam(teamId, "loss"),
+                this.gameService.getGameStatusByHomeTeam(teamId, "overtime_lose"),
+                this.gameService.getPointsByHomeTeam(teamId),
+                this.gameService.getGoalsForHomeTeam(teamId),
+                this.gameService.getGoalsAgainstHomeTeam(teamId),
+                this.gameService.getNumberOfGamesPlayedByHomeTeam(teamId)
+              ]).pipe(
+                map(data => {
+                  // @ts-ignore
+                  teamTest.wins = data[0];
+                  // @ts-ignore
+                  teamTest.loses = data[1];
+                  // @ts-ignore
+                  teamTest.overtimeLoses = data[2];
+                  // @ts-ignore
+                  teamTest.points = data[3];
+                  // @ts-ignore
+                  teamTest.goalsFor = data[4];
+                  // @ts-ignore
+                  teamTest.goalsAgainst = data[5];
+                  // @ts-ignore
+                  teamTest.gamesPlayed = data[6];
+                  return teamTest;
+                })
+              )
+            }),
+            toArray()
+          );
+        })
+      )
+      .subscribe(value => {
+        this.teamTest = value;
+        console.log(value)
+      });
+  }
+
+
+  showAwayTableOfTeams() {
+    this.teamService.getTeamsList()
+      .pipe(
+        mergeMap(res => {
+          return from(res).pipe(
+            mergeMap(teamTest => {
+
+              // @ts-ignore
+              const {teamId} = teamTest;
+              return forkJoin([
+                this.gameService.getGameStatusByAwayTeam(teamId, "win"),
+                this.gameService.getGameStatusByAwayTeam(teamId, "loss"),
+                this.gameService.getGameStatusByAwayTeam(teamId, "overtime_lose"),
+                this.gameService.getPointsByAwayTeam(teamId),
+                this.gameService.getGoalsForAwayTeam(teamId),
+                this.gameService.getGoalsAgainstAwayTeam(teamId),
+                this.gameService.getNumberOfGamesPlayedByAwayTeam(teamId)
               ]).pipe(
                 map(data => {
                   // @ts-ignore
